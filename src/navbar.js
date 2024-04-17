d021ebae64ef4ec8d9b92b7f304d8bca49c3bb73
Vaibhavi <135148044+Vaibhavi028@users.noreply.github.com>
2024-04-18 00:43:51 +0530

    Update navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
               <NavLink to="/">Home</NavLink>
             </li>
             <li>
-              <NavLink to="/researchs">Researchs</NavLink>
+              <NavLink to="/researchs">Researches</NavLink>
             </li>
             <li>
               <NavLink to="/surveys">Surveys</NavLink>
